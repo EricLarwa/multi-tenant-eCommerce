4c0a0fb947f7ef6c5bf7be8311be55e4c7a21046
EricLarwa <larwae22@students.ecu.edu>
2025-06-03 12:55:52 -0400

    Create components, adjust mapping for Landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.className} antialiased`}
-      >
+      <body className={`${dmSans.className} antialiased`}>
         {children}
       </body>
     </html>
